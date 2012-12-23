083172007125f2baed3033d1489d4be2405ab2a1
woky <woky@mailinator.com>
2012-12-23 15:25:35 +0100

    Imporve logging
    - Use Logback
    - Use bridge from Log4j
    - Optional debug logging (don't pollute /var/log/messages by default)

--- src/Alisa.scala
+++ src/Alisa.scala
@@ -8,12 +8,17 @@ import scala.collection.JavaConversions._
 import com.google.inject._
 import multibindings.Multibinder
 import sun.misc.{SignalHandler, Signal}
+import org.slf4j.{Logger => SLF4JLogger, LoggerFactory}
+import ch.qos.logback.classic.{Level, PatternLayout, LoggerContext, Logger => LBLogger}
+import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.classic.spi.ILoggingEvent
+import joptsimple.OptionParser
+import java.util.logging.{LogManager, Logger => JULLogger, Level => JULLevel}
 
 object Alisa extends Logger {
 
 	val DEFAULT_HANDLER_LISTS = IrcEventHandlerLists(command = IrcEventHandlerList(List(UnknownCommandHandler)))
 
-	SLF4JBridgeHandler.install()
 
 	def main(args: Array[String]) {
 		val termHandler = new SignalHandler {
@@ -25,11 +30,32 @@ object Alisa extends Logger {
 		Signal.handle(new Signal("TERM"), termHandler)
 		Signal.handle(new Signal("INT"), termHandler)
 
-		val config = if (args.length > 0) {
-			loadConfig(args(0))
-		} else {
-			TestConfig
-		}
+		val optParser = new OptionParser
+		val debugOpt = optParser.accepts("d", "Debug log level").withOptionalArg.ofType(classOf[Boolean])
+
+		val optSet = optParser.parse(args: _*)
+		val newArgs = optSet.nonOptionArguments
+
+		val verbose =
+			if (optSet.has(debugOpt)) {
+				if (optSet.hasArgument(debugOpt))
+					Some(optSet.valueOf(debugOpt))
+				else
+					Some(true)
+			} else {
+				None
+			}
+
+		initLogging(verbose.getOrElse(false))
+
+		var config =
+			if (newArgs.length > 0)
+				loadConfig(newArgs(0))
+			else
+				TestConfig
+
+		if (verbose.isDefined)
+			config = config.copy(global = config.global.copy(verbose = verbose.get))
 
 		// empty sets
 		val emptyMod = new AbstractModule {
@@ -73,4 +99,42 @@ object Alisa extends Logger {
 		conf
 	}
 
+	def initLogging(debug: Boolean) {
+		val lc = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
+		lc.reset
+
+		LogManager.getLogManager.reset
+		SLF4JBridgeHandler.install
+		JULLogger.getLogger(JULLogger.GLOBAL_LOGGER_NAME).setLevel(JULLevel.FINEST)
+
+		val listener = new ch.qos.logback.classic.jul.LevelChangePropagator
+		listener.setContext(lc)
+		lc.addListener(listener)
+
+		val layout = new PatternLayout
+		layout.setContext(lc)
+
+		val pattern =
+			if (debug)
+				"%d [%t] %-5p %c - %m%n"
+			else
+				"%p %c - %m%n"
+
+		layout.setPattern(pattern)
+		layout.start
+
+		val appender = new ConsoleAppender[ILoggingEvent]
+		appender.setContext(lc)
+		appender.setLayout(layout)
+		appender.start
+
+		val rootLogger = LoggerFactory.getLogger(SLF4JLogger.ROOT_LOGGER_NAME).asInstanceOf[LBLogger]
+		rootLogger.setLevel(Level.INFO)
+		rootLogger.addAppender(appender)
+
+		if (debug) {
+			val ourLogger = LoggerFactory.getLogger(getClass.getPackage.getName).asInstanceOf[LBLogger]
+			ourLogger.setLevel(Level.DEBUG)
+		}
+	}
 }
