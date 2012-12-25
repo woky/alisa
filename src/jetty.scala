e160b16d4e10513df6b5c83cd75934a689bc92cc
woky <woky@mailinator.com>
2012-12-25 15:45:45 +0100

    jetty: Use custom simple error handler

--- src/jetty.scala
+++ src/jetty.scala
@@ -1,11 +1,15 @@
 package alisa
 
 import com.google.inject.{Singleton, Injector, AbstractModule}
-import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.{Request, Server}
 import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
 import com.google.inject.servlet.{GuiceFilter, GuiceServletContextListener}
 import javax.inject.Inject
 import com.google.inject.multibindings.Multibinder
+import org.eclipse.jetty.server.handler.ErrorHandler
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
+import org.eclipse.jetty.http.{HttpStatus, MimeTypes, HttpMethod}
+import org.eclipse.jetty.util.ByteArrayISO8859Writer
 
 object Jetty {
 
@@ -33,6 +37,28 @@ final class JettyService @Inject()(config: JettyConfig, injector: Injector) exte
 		})
 		handler.addFilter(classOf[GuiceFilter], "/*", null)
 		handler.addServlet(classOf[DefaultServlet], "/")
+
+		handler.setErrorHandler(new ErrorHandler {
+			override def handle(target: String, baseRequest: Request, request: HttpServletRequest,
+			                    response: HttpServletResponse) {
+				baseRequest.setHandled(true)
+				val method = request.getMethod
+				if (!HttpMethod.GET.is(method) && !HttpMethod.POST.is(method) && !HttpMethod.HEAD.is(method))
+					return
+				response.setContentType(MimeTypes.Type.TEXT_PLAIN_8859_1.asString)
+				val writer = new ByteArrayISO8859Writer(64)
+
+				val status = response.getStatus
+				writer.write(status.toString)
+				writer.write(' ')
+				writer.write(HttpStatus.getMessage(status))
+
+				writer.flush
+				response.setContentLength(writer.size)
+				writer.writeTo(response.getOutputStream)
+				writer.destroy
+			}
+		})
 
 		val server = new Server(config.httpPort)
 		server.setHandler(handler)
