f0e3cc9c3f7ce42cee3b546eeafc797713d1f447
woky <nechtom@gmail.com>
2013-06-13 21:41:01 +0200

    Add JUnit test dependency

--- project/AlisaBuild.scala
+++ project/AlisaBuild.scala
@@ -21,12 +21,17 @@ object AlisaBuild extends Build {
 		"org.apache.lucene" % "lucene-analyzers-common" % "4.3.0",
 		"org.apache.lucene" % "lucene-queryparser" % "4.3.0",
 		"com.google.guava" % "guava" % "14.0.1",
-		"com.google.code.findbugs" % "jsr305" % "2.0.1" % "provided"
+		"com.google.code.findbugs" % "jsr305" % "2.0.1" % "provided",
 		/*
 		"org.scala-lang" % "scala-reflect" % scalaVer,
 		"net.java.dev.jna" % "jna" % "3.4.0",
 		"org.kohsuke" % "akuma" % "1.8",
 		*/
+
+		/*
+		 * Test dependencies
+		 */
+		"junit" % "junit" % "4.11" % "test"
 	)
 
 	lazy val proj = Project(
