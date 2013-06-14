cef7fa65833c65ce844ea4ec7587567a075096f1
woky <nechtom@gmail.com>
2013-06-15 01:17:55 +0200

    Add IrcChannelEvent

--- src/alisa/events.scala
+++ src/alisa/events.scala
@@ -9,6 +9,11 @@ trait IrcEvent {
 	def network: IrcNetwork
 }
 
+trait IrcChannelEvent extends IrcEvent {
+
+	def channel: String
+}
+
 case class IrcUser(nick: String, login: String, hostname: String)
 
 case class IrcText(text: String, decoded: String)
@@ -16,18 +21,20 @@ case class IrcText(text: String, decoded: String)
 case class IrcMessageEvent(network: IrcNetwork,
                            channel: String,
 						   user: IrcUser,
-                           message: IrcText) extends IrcEvent
+                           message: IrcText) extends IrcChannelEvent
 
 case class IrcCommandEvent(network: IrcNetwork,
                            channel: String,
 						   user: IrcUser,
                            command: String,
-                           args: IrcText) extends IrcEvent
+                           args: IrcText) extends IrcChannelEvent
 
 case class IrcActionEvent(network: IrcNetwork,
 						  user: IrcUser,
                           target: String,
-                          action: IrcText) extends IrcEvent
+                          action: IrcText) extends IrcChannelEvent {
+	def channel = target
+}
 
 case class IrcPrivMsgEvent(network: IrcNetwork,
 						   user: IrcUser,
@@ -35,8 +42,8 @@ case class IrcPrivMsgEvent(network: IrcNetwork,
 
 case class IrcJoinEvent(network: IrcNetwork,
                         channel: String,
-                        user: IrcUser) extends IrcEvent
+                        user: IrcUser) extends IrcChannelEvent
 
 case class IrcPartEvent(network: IrcNetwork,
                         channel: String,
-                        user: IrcUser) extends IrcEvent
+                        user: IrcUser) extends IrcChannelEvent
