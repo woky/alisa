3d4e548ff9319bdd2ed8ee8cf13887fef4c83595
woky <nechtom@gmail.com>
2013-06-15 11:49:37 +0200

    Add IrcUserEvent

--- src/alisa/events.scala
+++ src/alisa/events.scala
@@ -4,6 +4,8 @@ import org.jibble.pircbot.PircBot
 
 case class IrcNetwork(name: String, bot: PircBot)
 
+case class IrcUser(nick: String, login: String, hostname: String)
+
 trait IrcEvent {
 
 	def network: IrcNetwork
@@ -14,7 +16,10 @@ trait IrcChannelEvent { this: IrcEvent =>
 	def channel: String
 }
 
-case class IrcUser(nick: String, login: String, hostname: String)
+trait IrcUserEvent { this: IrcEvent =>
+
+	def user: IrcUser
+}
 
 case class IrcText(text: String, decoded: String)
 
@@ -22,35 +27,36 @@ case class IrcMessageEvent(network: IrcNetwork,
                            channel: String,
 						   user: IrcUser,
                            message: IrcText)
-		extends IrcEvent with IrcChannelEvent
+		extends IrcEvent with IrcChannelEvent with IrcUserEvent
 
 case class IrcCommandEvent(network: IrcNetwork,
                            channel: String,
 						   user: IrcUser,
                            command: String,
                            args: IrcText)
-		extends IrcEvent with IrcChannelEvent
+		extends IrcEvent with IrcChannelEvent with IrcUserEvent
 
 case class IrcActionEvent(network: IrcNetwork,
 						  user: IrcUser,
                           target: String,
                           action: IrcText)
-		extends IrcEvent with IrcChannelEvent {
+		extends IrcEvent with IrcChannelEvent with IrcUserEvent {
 
 	def channel = target
 }
 
 case class IrcPrivMsgEvent(network: IrcNetwork,
 						   user: IrcUser,
-						   message: IrcText) extends IrcEvent
+						   message: IrcText)
+		extends IrcEvent with IrcUserEvent
 
 case class IrcJoinEvent(network: IrcNetwork,
                         channel: String,
                         user: IrcUser)
-		extends IrcEvent with IrcChannelEvent
+		extends IrcEvent with IrcChannelEvent with IrcUserEvent
 
 
 case class IrcPartEvent(network: IrcNetwork,
                         channel: String,
                         user: IrcUser)
-		extends IrcEvent with IrcChannelEvent
+		extends IrcEvent with IrcChannelEvent with IrcUserEvent
