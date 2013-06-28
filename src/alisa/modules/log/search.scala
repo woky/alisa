2c30b0f52b236d90531a6904190e12b00bd01703
woky <woky@mailinator.com>
2013-06-28 16:03:12 +0200

    log: Replace JDK http server with Netty
    JDK http server fails on invalid URI syntax but we want just valid URL
    for special Lucene search characters.

---
+++ src/alisa/modules/log/search.scala
@@ -0,0 +1,196 @@
+package alisa.modules.log
+
+import io.netty.channel._
+import io.netty.handler.codec.http._
+
+import io.netty.handler.codec.http.HttpHeaders.Names._
+import io.netty.handler.codec.http.HttpHeaders._
+import io.netty.handler.codec.http.HttpResponseStatus._
+import io.netty.handler.codec.http.HttpVersion._
+import alisa.util.Logger
+import io.netty.buffer.Unpooled
+import io.netty.util.CharsetUtil._
+import scala.collection.JavaConversions._
+import io.netty.handler.stream.{ChunkedWriteHandler, ChunkedMessageInput}
+import org.apache.lucene.queryparser.classic.ParseException
+import scala.Some
+import java.text.SimpleDateFormat
+import java.util.Date
+import io.netty.channel.socket.SocketChannel
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.socket.nio.NioServerSocketChannel
+import java.net.InetSocketAddress
+
+/*
+ * TODO
+ * It seems that HttpObjectAggregator doesn't work because each message is
+ * sent in separate HTTP chunk or we don't use it correctly.
+ */
+
+final class SearchServer(bindAddr: InetSocketAddress, allowedIds: AllowedIds,
+                         lucene: LuceneService) {
+
+	private val bossGroup = new NioEventLoopGroup
+	private val workerGroup = new NioEventLoopGroup
+	private var channel: Option[Channel] = start
+
+	private def start: Option[Channel] = {
+		try {
+			val b = new ServerBootstrap
+			b.option(ChannelOption.SO_BACKLOG.asInstanceOf[ChannelOption[Any]], 64)
+			b.group(bossGroup, workerGroup)
+			b.channel(classOf[NioServerSocketChannel])
+			b.childHandler(new ChannelInitializer[SocketChannel] {
+				def initChannel(ch: SocketChannel) {
+					val p = ch.pipeline
+					p.addLast("decoder", new HttpRequestDecoder)
+					p.addLast("aggregator", new HttpObjectAggregator(65536))
+					p.addLast("encoder", new HttpResponseEncoder())
+					p.addLast("chunkedWriter", new ChunkedWriteHandler())
+					p.addLast("handler", new SearchHandler(allowedIds, lucene))
+				}
+			})
+			val bf = b.bind(bindAddr)
+			try {
+				Some(bf.sync.channel)
+			} catch {
+				case e: Exception =>
+					bf.channel.close.sync
+					throw e
+			}
+		} catch {
+			case e: Exception =>
+				bossGroup.shutdownGracefully()
+				workerGroup.shutdownGracefully()
+				throw e
+		}
+	}
+
+	def stop {
+		synchronized {
+			channel match {
+				case Some(ch) =>
+					ch.close.sync
+					bossGroup.shutdownGracefully()
+					workerGroup.shutdownGracefully()
+					channel = None
+				case _ =>
+			}
+		}
+	}
+}
+
+private object SearchHandler {
+
+	val OOPS_STR = "¯\\_(ツ)_/¯"
+	val CT_TEXT = "text/plain; charset=utf-8"
+
+	val KEY_QUERY = "query"
+	val KEY_BYTIME = "sortByTime"
+	val KEY_LIMIT = "limit"
+
+	val DEF_QUERY = "*"
+	val DEF_BYTIME = true
+	val DEF_LIMIT = 50
+}
+
+private final class SearchHandler(allowedIds: AllowedIds, lucene: LuceneService)
+		extends SimpleChannelInboundHandler[Object] with Logger {
+
+	import SearchHandler._
+
+	def messageReceived(ctx: ChannelHandlerContext, msg: Object) {
+		if (!msg.isInstanceOf[HttpRequest]) {
+			logWarn("Unknown channel message: " + msg.getClass)
+			return
+		}
+
+		val req = msg.asInstanceOf[HttpRequest]
+		if (req.getMethod != HttpMethod.GET) {
+			sendError(ctx, UNAUTHORIZED)
+			return
+		}
+
+		val queryDec = new QueryStringDecoder(req.getUri)
+		val path = queryDec.path
+		if (path.length != allowedIds.idLen + 1) {
+			sendError(ctx, UNAUTHORIZED)
+			return
+		}
+
+		val luceneChan = allowedIds(path.substring(1)) match {
+			case Some(ch) => ch
+			case _ =>
+				sendError(ctx, UNAUTHORIZED)
+				return
+		}
+
+		val params = queryDec.parameters.map({
+			case (k, v) => k -> v(0)
+		})
+		val query = params.getOrElse(KEY_QUERY, DEF_QUERY)
+		val limit = params.get(KEY_LIMIT).map(_.toInt).getOrElse(DEF_LIMIT)
+		val byTime = params.get(KEY_BYTIME).map(_.toBoolean).getOrElse(DEF_BYTIME)
+
+		try {
+			val sParams = LuceneSearchParams(limit, byTime)
+			// XXX search in different thread?
+			val results = lucene.search(query, luceneChan, sParams)
+
+			val resp = new DefaultHttpResponse(HTTP_1_1, OK)
+			resp.headers.set(CONTENT_TYPE, CT_TEXT)
+			resp.headers.set(TRANSFER_ENCODING, Values.CHUNKED)
+
+			val msgList = MessageList.newInstance[Object]
+			msgList.add(resp)
+			msgList.add(new SearchChunkedInput(results.iterator))
+			msgList.add(LastHttpContent.EMPTY_LAST_CONTENT)
+
+			ctx.write(msgList).addListener(ChannelFutureListener.CLOSE)
+		} catch {
+			case e: ParseException => sendError(ctx, BAD_REQUEST, Some(e.getMessage))
+			case e: Exception => logError("lucene.search() failed", e)
+		}
+	}
+
+	def sendError(ctx: ChannelHandlerContext, status: HttpResponseStatus,
+	              optMsg: Option[String] = None) {
+		val statusMsg = status.toString + " " + OOPS_STR + "\n"
+		val msg = optMsg match {
+			case Some(s) => statusMsg + s
+			case _ => statusMsg
+		}
+		val buf = Unpooled.copiedBuffer(msg, UTF_8)
+		val resp = new DefaultFullHttpResponse(HTTP_1_1, status, buf)
+		resp.headers.set(CONTENT_TYPE, CT_TEXT)
+		ctx.write(resp).addListener(ChannelFutureListener.CLOSE)
+	}
+}
+
+private final class SearchChunkedInput(it: Iterator[LuceneStoredMessage])
+		extends ChunkedMessageInput[HttpContent] {
+
+	private val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm")
+
+	def isEndOfInput = !it.hasNext
+
+	def close() {}
+
+	private def formatMsg(msg: LuceneStoredMessage) = {
+		val time = dateFormat.format(new Date(msg.stored.time))
+		s"$time <${msg.stored.nick}> ${msg.stored.message}\n"
+	}
+
+	def readChunk(buffer: MessageList[HttpContent]) = {
+		if (it.hasNext) {
+			val msg = formatMsg(it.next)
+			val buf = Unpooled.copiedBuffer(msg, UTF_8)
+			val content = new DefaultHttpContent(buf)
+			buffer.add(content)
+			true
+		} else {
+			false
+		}
+	}
+}
