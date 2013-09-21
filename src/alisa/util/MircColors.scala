43a3b2024f45ada46e375c011407068ecaf15635
woky <nechtom@gmail.com>
2013-09-21 20:45:40 +0200

    MircColors: Store mIRC color codes as String
    Integer values are not needed

--- src/alisa/util/MircColors.scala
+++ src/alisa/util/MircColors.scala
@@ -8,24 +8,24 @@ object MircColors {
 	val INVERSE		= '\u0016'
 	val CLEAR		= '\u000F'
 
-	val WHITE		= 0
-	val BLACK		= 1
-	val BLUE		= 2
-	val GREEN		= 3
-	val RED			= 4
-	val BROWN		= 5
-	val PURPLE		= 6
-	val ORANGE		= 7
-	val YELLOW		= 8
-	val LIGHT_GREEN	= 9
-	val TEAL		= 10
-	val LIGHT_CYAN	= 11
-	val LIGHT_BLUE	= 12
-	val PINK		= 13
-	val GREY		= 14
-	val LIGHT_GREY	= 15
+	val WHITE		= "00"
+	val BLACK		= "01"
+	val BLUE		= "02"
+	val GREEN		= "03"
+	val RED			= "04"
+	val BROWN		= "05"
+	val PURPLE		= "06"
+	val ORANGE		= "07"
+	val YELLOW		= "08"
+	val LIGHT_GREEN	= "09"
+	val TEAL		= "10"
+	val LIGHT_CYAN	= "11"
+	val LIGHT_BLUE	= "12"
+	val PINK		= "13"
+	val GREY		= "14"
+	val LIGHT_GREY	= "15"
 
-	def apply(fg: Int): String = COLOR_CODE + "%02d".format(fg)
+	def apply(fg: String): String = COLOR_CODE + fg
 
-	def apply(fg: Int, bg: Int): String = COLOR_CODE + "%02d,%02d".format(fg, bg)
+	def apply(fg: String, bg: String): String = COLOR_CODE + fg + ',' + bg
 }
