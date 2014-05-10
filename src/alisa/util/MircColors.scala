bfcb0c50fa12d33d325b602af407a9717a0e506f
woky <nechtom@gmail.com>
2014-05-10 12:44:09 +0200

    Make colors codes real constants

--- src/alisa/util/MircColors.scala
+++ src/alisa/util/MircColors.scala
@@ -2,28 +2,28 @@ package alisa.util
 
 object MircColors {
 
-	val COLOR_CODE	= '\u0003'
-	val BOLD		= '\u0002'
-	val UNDERLINE	= '\u001F'
-	val INVERSE		= '\u0016'
-	val CLEAR		= '\u000F'
+	final val COLOR_CODE    = '\u0003'
+	final val BOLD          = '\u0002'
+	final val UNDERLINE     = '\u001F'
+	final val INVERSE       = '\u0016'
+	final val CLEAR         = '\u000F'
 
-	val WHITE		= "00"
-	val BLACK		= "01"
-	val BLUE		= "02"
-	val GREEN		= "03"
-	val RED			= "04"
-	val BROWN		= "05"
-	val PURPLE		= "06"
-	val ORANGE		= "07"
-	val YELLOW		= "08"
-	val LIGHT_GREEN	= "09"
-	val TEAL		= "10"
-	val LIGHT_CYAN	= "11"
-	val LIGHT_BLUE	= "12"
-	val PINK		= "13"
-	val GREY		= "14"
-	val LIGHT_GREY	= "15"
+	final val WHITE         = "00"
+	final val BLACK         = "01"
+	final val BLUE          = "02"
+	final val GREEN         = "03"
+	final val RED           = "04"
+	final val BROWN         = "05"
+	final val PURPLE        = "06"
+	final val ORANGE        = "07"
+	final val YELLOW        = "08"
+	final val LIGHT_GREEN   = "09"
+	final val TEAL          = "10"
+	final val LIGHT_CYAN    = "11"
+	final val LIGHT_BLUE    = "12"
+	final val PINK          = "13"
+	final val GREY          = "14"
+	final val LIGHT_GREY    = "15"
 
 	def apply(fg: String): String = COLOR_CODE + fg
 
