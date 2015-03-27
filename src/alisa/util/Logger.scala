ed26973cf00bfcab2bb10d7d1881d5a493408f0b
woky <nechtom@gmail.com>
2015-03-27 21:58:23 +0100

    Refactor Logger trait
    Log exception in all levels.

--- src/alisa/util/Logger.scala
+++ src/alisa/util/Logger.scala
@@ -1,31 +1,24 @@
 package alisa.util
 
 import java.util.logging.{Level, Logger => JDKLogger}
+import Level._
 
 trait Logger {
 
 	// XXX maybe use just one global instance?
 	private final val logger = JDKLogger.getLogger(getClass.getCanonicalName)
 
-	protected final def logDebug(msg: => String, ex: Throwable = null) {
-		if (logger.isLoggable(Level.FINE))
-			logger.fine(logMsg(msg))
-	}
+	protected final def logDebug(msg: => String, ex: Throwable = null) = _log(FINE, msg, ex)
 
-	protected final def logInfo(msg: => String, ex: Throwable = null) {
-		if (logger.isLoggable(Level.INFO))
-			logger.info(logMsg(msg))
-	}
+	protected final def logInfo(msg: => String, ex: Throwable = null) = _log(INFO, msg, ex)
 
-	protected final def logWarn(msg: => String, ex: Throwable = null) {
-		if (logger.isLoggable(Level.WARNING))
-			logger.log(Level.WARNING, msg, ex)
-	}
+	protected final def logWarn(msg: => String, ex: Throwable = null) = _log(WARNING, msg, ex)
 
-	protected final def logError(msg: => String, ex: Throwable = null) {
-		if (logger.isLoggable(Level.SEVERE))
-			logger.log(Level.SEVERE, msg, ex)
-	}
+	protected final def logError(msg: => String, ex: Throwable = null) = _log(SEVERE, msg, ex)
+
+	private def _log(level: Level, msg: => String, ex: Throwable = null): Unit =
+		if (logger.isLoggable(level)) // needs to be checked before msg is evaluated
+			logger.log(level, msg, ex)
 
 	protected def logMsg(msg: => String): String = msg
 }
